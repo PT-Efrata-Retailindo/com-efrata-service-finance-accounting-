6799199663bdfc1fc5f1e768ab7518cd5dca9bed
MatthewADIAN <hartonomatthew0@gmail.com>
2023-09-08 07:55:49 +0700

    Enhance PDF Template

--- Com.Efrata.Service.Finance.Accounting.Lib/PDFTemplates/VBRealizationDocumentNonPOPDFTemplate.cs
+++ Com.Efrata.Service.Finance.Accounting.Lib/PDFTemplates/VBRealizationDocumentNonPOPDFTemplate.cs
@@ -703,10 +703,10 @@ namespace Com.Efrata.Service.Finance.Accounting.Lib.PDFTemplates
             table.AddCell(cell);
             cell.Phrase = new Phrase("(..................)", normal_font);
             table.AddCell(cell);
-            cell.Phrase = new Phrase("(..................)", normal_font);
-            table.AddCell(cell);
+            cell.Colspan = 2;
             cell.Phrase = new Phrase($"(..................)", normal_font);
             table.AddCell(cell);
+            cell.Colspan = 0;
             cell.Phrase = new Phrase($"({viewModel.CreatedBy})", normal_font);
             table.AddCell(cell);
             document.Add(table);
